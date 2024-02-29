aee4155f94c0c090ee3f3809815b2b37d2dc845b
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-29 19:49:00 +0200

    Webpack

---
+++ src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const _ = require('lodash');
+
+const randomNumber = Number(_.random(1, 10, true).toFixed(2));
+
+console.log('--->>> START <<<---');
+console.log(randomNumber);
+console.log('--->>> END <<<---');
+
+const div = document.queryselector('#root');
+div.append('HELLO WEBPACK');
+
+/*
+
+App.js ->         |
+Dashboard.js ->   | Bundler (Webpack) | bundle.js
+About.js ->       |
+
+Бандл - код різних модулей, зібраний в одному файлі
+
+
+1. Production
+2. Development  
+
+*/
